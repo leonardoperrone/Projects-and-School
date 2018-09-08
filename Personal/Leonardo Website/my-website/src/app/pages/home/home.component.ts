b18b253500e308edc9fdff6a4acb3e15741f42c6
Leonardo Perrone <liox2012@gmail.com>
2018-09-08 18:49:55 +0200

    Tooltip and routing changes

--- Personal/Leonardo Website/my-website/src/app/pages/home/home.component.ts
+++ Personal/Leonardo Website/my-website/src/app/pages/home/home.component.ts
@@ -12,7 +12,7 @@ export class HomeComponent implements OnInit {
       {
         name: 'javascript',
         logos: ['/assets/javascript-logo.png', '/assets/javascript-logo.png'],
-        description: ' Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+        description: 'I had the chance to develop E2E, integration, and unit tests during my first 6 months internship and I enjoyed the scalability that these tests offer in terms of quality assurance. I believe testing is key to proper user experience and to deliver quality products. I think any application should have some automation in place whether or not the application will grow.',
         svgSpecs: {
           rotation: '61',
           dash: '191.5399932861328',
